ff7dd12da2a73a02eeb076d1b5da1c5f697779a3
Aaron Contreras <aaronlcaq@gmail.com>
2023-10-06 14:01:13 -0500

    Promote `HavingEntityMembership` to the `Principal` level
    Allows querying for entity memberships on groups as well as users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -77,12 +77,6 @@ class User < Principal
            inverse_of: :user,
            dependent: :destroy
 
-  has_many :work_package_shares,
-           -> { where(entity_type: WorkPackage.name) },
-           class_name: 'Member',
-           dependent: :delete_all,
-           inverse_of: :principal
-
   has_many :notification_settings, dependent: :destroy
 
   # Users blocked via brute force prevention
@@ -95,8 +89,7 @@ class User < Principal
          :notified_globally,
          :watcher_recipients,
          :with_time_zone,
-         :having_reminder_mail_to_send,
-         :having_entity_membership
+         :having_reminder_mail_to_send
 
   def self.create_blocked_scope(scope, blocked)
     scope.where(blocked_condition(blocked))
